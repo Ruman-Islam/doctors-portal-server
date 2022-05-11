f538c26de0bdef88fbb9f6487c291ee524860b31
Ruman-Islam <rumanislam48@gmail.com>
2022-05-11 22:32:58 +0600

    service collection api created

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vzdnu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const appointmentsCollection = client.db("doctors-portal").collection("appointments");
+        const servicesCollection = client.db("doctors-portal").collection("services");
+
+        app.get('/services', async (req, res) => {
+            const query = req.query;
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
@@ -13,3 +41,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Doctors Portal server is running on port -', port);
 })
+
+// http://localhost:5000/
